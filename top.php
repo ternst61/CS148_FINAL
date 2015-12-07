1a67820d378e23992ef55a6f0414ae0f73a7a32c
skswanke <skswanke@gmail.com>
2015-12-07 13:55:20 -0500

    fix top

--- top.php
+++ top.php
@@ -1,3 +1,7 @@
+<?php
+include "lib/constants.php";
+require_once('lib/custom-functions.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,47 +21,110 @@
 <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
 <link rel="stylesheet" href="css/extra.css" type="text/css" media="screen">
 
-<?php
-// %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
-//
-// PATH SETUP
-//
-//  $domain = "https://www.uvm.edu" or http://www.uvm.edu;
- $domain = "http://";
-if (isset($_SERVER['HTTPS'])) {
-    if ($_SERVER['HTTPS']) {
-        $domain = "https://";
-    }
-}
+        <?php
+        $debug = false;
 
-$server = htmlentities($_SERVER['SERVER_NAME'], ENT_QUOTES, "UTF-8");
+        // %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+        //
+        // inlcude all libraries. Note some are in lib and some are in bin
+        // bin should be located at the same level as www-root (it is not in 
+        // github)
+        // 
+        // yourusername
+        //     bin
+        //     www-logs
+        //     www-root
+        
+        
+        $includeDBPath = "../bin/";
+        $includeLibPath = "../lib/";
+        
+        
+        require_once($includeLibPath . 'mailMessage.php');
 
-$domain .= $server;
+        require_once('lib/security.php');
+        
+        require_once($includeDBPath . 'Database.php');
+        
+        // %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+        //
+        // PATH SETUP
+        //  
+            
+        // sanitize the server global variable
+        $_SERVER = filter_input_array(INPUT_SERVER, FILTER_SANITIZE_STRING);
+        foreach ($_SERVER as $key => $value) {
+            $_SERVER[$key] = sanitize($value, false);
+        }
+        
+        $domain = "//"; // let the server set http or https as needed
 
-$phpSelf = htmlentities($_SERVER['PHP_SELF'], ENT_QUOTES, "UTF-8");
+        $server = htmlentities($_SERVER['SERVER_NAME'], ENT_QUOTES, "UTF-8");
 
-$path_parts = pathinfo($phpSelf);
+        $domain .= $server;
 
-if ($debug){
-    print "<p>Domain" . $domain;
-    print "<p>php Self". $phpSelf;
-    print "<p>Path Parts<pre>";
-    print_r($path_parts);
-    print "</pre>";
-}
+        $phpSelf = htmlentities($_SERVER['PHP_SELF'], ENT_QUOTES, "UTF-8");
 
-// %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
-//
-// inlcude all libraries
-//
+        $path_parts = pathinfo($phpSelf);
 
-require_once('lib/security.php');
+        if ($debug) {
+            print "<p>Domain" . $domain;
+            print "<p>php Self" . $phpSelf;
+            print "<p>Path Parts<pre>";
+            print_r($path_parts);
+            print "</pre>";
+        }
+        
+        $yourURL = $domain . $phpSelf;
 
-if ($path_parts['filename'] == "form") {
-    include "lib/validation-functions.php";
-    include "lib/mail-message.php";
-}
-?>	
+        // %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+        // sanatize global variables 
+        // function sanitize($string, $spacesAllowed)
+        // no spaces are allowed on most pages but your form will most likley
+        // need to accept spaces. Notice my use of an array to specfiy whcih 
+        // pages are allowed.
+        // generally our forms dont contain an array of elements. Sometimes
+        // I have an array of check boxes so i would have to sanatize that, here
+        // i skip it.
+
+        $spaceAllowedPages = array("form.php");
+
+        if (!empty($_GET)) {
+            $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_STRING);
+            foreach ($_GET as $key => $value) {
+                $_GET[$key] = sanitize($value, false);
+            }
+        }
+        
+        // %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+        //
+        // Process security check.
+        //
+        
+        if (!securityCheck($path_parts, $yourURL)) {
+            print "<p>Login failed: " . date("F j, Y") . " at " . date("h:i:s") . "</p>\n";
+            die("<p>Sorry you cannot access this page. Security breach detected and reported</p>");
+        }
+
+        // %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+        //
+        // Set up database connection
+        //
+        
+        $dbUserName = get_current_user() . '_reader';
+        $whichPass = "r"; //flag for which one to use.
+        $dbName = DATABASE_NAME;
+
+        $thisDatabaseReader = new Database($dbUserName, $whichPass, $dbName);
+        
+        $dbUserName = get_current_user() . '_writer';
+        $whichPass = "w";
+        $thisDatabaseWriter = new Database($dbUserName, $whichPass, $dbName);
+        
+        $dbUserName = get_current_user() . '_admin';
+        $whichPass = "a";
+        $thisDatabaseAdmin = new Database($dbUserName, $whichPass, $dbName);
+        ?>  
 
 </head>
 <!-- ################ body section ######################### -->
